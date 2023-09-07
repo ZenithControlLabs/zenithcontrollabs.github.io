12d3253048ac3cee7972dd6ae52898a4574d6d7a
panther03 <tripod320@gmail.com>
2023-09-07 20:41:21 +0200

    settings & button remap

--- Phobri64_GUI/main.js
+++ Phobri64_GUI/main.js
@@ -1,12 +1,20 @@
-import { connect } from "../lib/modules/cntlr.js";
+import { connect, saveSettings, resetSettings } from "../lib/modules/cntlr.js";
 import { prevStep, nextStep, startCalib } from "../lib/modules/zenith_calib.js";
+import { setCommsMode } from "../lib/modules/zenith_remap.js";
 
 // functions exported to the HTML world
 window._fns = {
     connect,
     prevStep,
     nextStep,
-    startCalib
+    startCalib,
+    saveSettings,
+    resetSettings,
+    setCommsMode
 };
+
+///////////
+// INIT //
+/////////
 
 document.getElementById("app-title").textContent = `${productName} Config Tool`
--- Phobri64_GUI/zenith_cfg.js
+++ Phobri64_GUI/zenith_cfg.js
@@ -8,3 +8,38 @@ const devVID = 0x7318;
 const devPID = 0x0000;
 const vendorName = "Zenith Control Labs";
 const productName = "Phobri64";
+
+const buttonNames = [
+    "A",
+    "B",
+    "CU",
+    "CD",
+    "CL",
+    "CR",
+    "Start",
+    "L",
+    "R",
+    "ZL",
+    "DD",
+    "DL",
+    "DR",
+    "DU",
+    "ZR",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+]  
